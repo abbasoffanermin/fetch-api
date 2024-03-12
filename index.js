5bc8622259a067a99dc30c75fefb1b3d32104502
abbasoffanermin <105522485+abbasoffanermin@users.noreply.github.com>
2024-03-12 23:19:41 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,152 @@
+//1--> https://simple-grocery-store-api.glitch.me/products
+
+// fetch('https://simple-grocery-store-api.glitch.me/products')
+// .then(response => response.json())
+// .then(data => {
+//   console.log(data)
+// })
+// .catch(error => {
+//   console.log(error)
+// })
+//---------------------------------------------------------------------------------------
+
+// const callApi1= async()=>{
+//   const response = await fetch('https://simple-grocery-store-api.glitch.me/products');
+//   const data=await response.json()
+//   .then(data => {
+//     console.log(data)
+//   })
+//   .catch(error => {
+//     console.log(error)
+//   })
+// }
+// callApi1();
+
+//2 -->https://simple-grocery-store-api.glitch.me/carts
+
+// fetch('https://simple-grocery-store-api.glitch.me/carts')
+// .then(response=>response.json())
+// .then(data=>console.log(data))
+// .catch(error=>console.log(error))
+
+//-----------------------------------------------------------------------------
+// const callApi2=async()=>{
+//   const response = await fetch('https://simple-grocery-store-api.glitch.me/carts');
+//   const data=await response.json()
+//   .then(data => {
+//     console.log(data)
+//   })
+// }
+// callApi2()
+
+
+
+//3-->https://simple-grocery-store-api.glitch.me/orders
+// fetch('https://simple-grocery-store-api.glitch.me/orders')
+// .then(response=>response.json())
+// .then(data=>console.log(data))
+
+// const calApi3=async ()=>{
+//   const response=await fetch('https://simple-grocery-store-api.glitch.me/orders')
+//   const data=await response.json()
+//   .then(data=>console.log(data))
+// }
+// calApi3()
+
+
+
+//4-->http://www.boredapi.com/api/activity/
+
+// fetch('http://www.boredapi.com/api/activity/')
+// .then(response=>response.json())
+// .then(data=>console.log(data))
+
+
+// const calApi4=async ()=>{
+//   const response=await fetch('http://www.boredapi.com/api/activity/')
+//   const data=await response.json()
+//   .then(data=>console.log(data))
+// }
+// calApi4()
+
+//5-->https://api.thecatapi.com/v1/images/0XYvRd7oD
+
+// fetch('https://api.thecatapi.com/v1/images/0XYvRd7oD')
+// .then(response=>response.json())
+// .then(data=>console.log(data))
+
+
+// const callApi5=async ()=>{
+//   const response=await fetch('https://api.thecatapi.com/v1/images/0XYvRd7oD')
+//   const data=await response.json()
+//   .then(data=>console.log(data))
+// }
+// callApi5()
+
+//6-->https://pokeapi.co/api/v2/pokemon/ditto
+
+// fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+// .then(response=>response.json())
+// .then(data=>console.log(data))
+
+
+// const callApi6=async ()=>{
+//   const response=await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+//   const data=await response.json()
+//   .then(data=>console.log(data))
+// }
+// callApi6()
+
+//7-->https://api.nasa.gov/planetary/apod
+
+// fetch('https://api.nasa.gov/planetary/apod')
+// .then(response=>response.json())
+// .then(data=>console.log(data))
+
+
+// const callApi7=async ()=>{
+//   const response=await fetch('https://api.nasa.gov/planetary/apod')
+//   const data=await response.json()
+//   .then(data=>console.log(data))
+// }
+// callApi7()
+
+// 8-->https://fakestoreapi.com/products
+
+// fetch('https://fakestoreapi.com/products')
+// .then(response=>response.json())
+// .then(data=>console.log(data))
+
+// const callApi8=async ()=>{
+//   const response=await fetch('https://fakestoreapi.com/products')
+//   const data=await response.json()
+//   .then(data=>console.log(data))
+// }
+// callApi8()
+//9-->https://fakestoreapi.com/carts
+
+// fetch('https://fakestoreapi.com/carts')
+// .then(response=>response.json())
+// .then(data=>console.log(data))
+
+
+// const callApi9=async ()=>{
+//   const response=await fetch('https://fakestoreapi.com/carts')
+//   const data=await response.json()
+//   .then(data=>console.log(data))
+// }
+// callApi9()
+
+//10-->https://fakestoreapi.com/users
+
+// fetch('https://fakestoreapi.com/users')
+// .then(response=>response.json())
+// .then(data=>console.log(data))
+
+
+// const callApi10=async ()=>{
+//   const response=await fetch('https://fakestoreapi.com/users')
+//   const data=await response.json()
+//   .then(data=>console.log(data))
+// }
+// callApi10()
